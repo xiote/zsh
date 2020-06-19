95d95ac7f7cdfb307d36f8f54b206b3018435e88
xiote <xiote@nate.com>
2020-06-20 02:45:00 +0900

    설치 스크립트 추가

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo "alias src='. ~/.zshrc'" >> ~/.zshrc
+echo "rm ~/.MyZshrc" >> ~/.zshrc
+echo "curl -L https://raw.githubusercontent.com/xiote/zsh/master/.zshrc -o ~/.MyZshrc;" >> ~/.zshrc
+echo "source ~/.MyZshrc" >> ~/.zshrc
+echo "export LC_ALL=C.UTF-8" >> ~/.zshrc
