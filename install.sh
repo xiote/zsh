daca76ea00a0ea0a650b43f7f6bf602675a89c12
xiote <xiote@nate.com>
2020-06-23 18:56:41 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ echo "alias src='. ~/.zshrc'" >> ~/.zshrc
 echo "rm ~/.MyZshrc" >> ~/.zshrc
 echo "curl -L https://raw.githubusercontent.com/xiote/zsh/master/.zshrc -o ~/.MyZshrc;" >> ~/.zshrc
 echo "source ~/.MyZshrc" >> ~/.zshrc
-echo "export LC_ALL=C.UTF-8" >> ~/.zshrc
+echo "export LC_ALL=ko_KR.UTF-8" >> ~/.zshrc
+echo "export LANG=ko_KR.UTF-8" >> ~/.zshrc
